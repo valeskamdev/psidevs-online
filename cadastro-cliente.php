f143c6b3bc20384050bde252b20f27a218115602
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-10-04 11:43:54 -0300

    Ajuste cadastro-cliente

--- cadastro-cliente.php
+++ cadastro-cliente.php
@@ -45,6 +45,11 @@
                         <label for="senha-cliente" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Senha</label>
                     </div>
 
+                    <div class="flex items-center mb-4">
+                        <input id="lembrar-senha" type="checkbox" value="lembrar-senha" name="lembrar-senha" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <label for="lembrar-senha" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300" style="font-size: 1rem;">Lembrar Senha</label>
+                    </div>
+
                     <div class="relative max-w-sm">
 
                         <input datepicker datepicker-title="Flowbite datepicker" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date">
@@ -52,13 +57,11 @@
 
 
                     <div class="text-center">
-                        <button type="submit" name="cadastro-cliente" class="button-cadastro-cliente"><b>Enviar</b></button>
+                        <button type="submit" name="cadastro-cliente" class="button-cadastro-cliente"><b>CRIAR CONTA</b></button>
+                        <p> <a href="">Já tem conta?</a> | <a href="esqueceu-senha.php">Esqueceu a Senha?</a> </p>
                     </div>
 
-                    <div class="flex items-center mb-4">
-                        <input id="lembrar-senha" type="checkbox" value="lembrar-senha" name="lembrar-senha" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="lembrar-senha" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Lembrar Senha</label>
-                    </div>
+                  
             </div>
             </form>
         </div>
