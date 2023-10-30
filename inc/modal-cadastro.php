4e4905610897afbad5552846c98896e0fa450e50
Melissa Julia L. L <108154398+melissaJll@users.noreply.github.com>
2023-10-30 12:00:36 -0300

    arrumando modal para mobile

--- inc/modal-cadastro.php
+++ inc/modal-cadastro.php
@@ -5,12 +5,12 @@
 
             <div id="modal-cadastro" class ="modal hide">
                     <!-- Modal cabeçalho -->
-                    <div class="modal-header flex items-start justify-center p-6 border-b rounded-t dark:border-gray-600 ">
-                        <div class="mx-auto text-center" >
-                            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    <div class="modal-header flex items-start justify-center p-6 border-b rounded-t dark:border-gray-600 sm:text-lg">
+                        <div class="mx-auto text-center lg:p-3" >
+                            <h3 class="text-xl font-semibold text-gray-900 dark:text-white lg:text-3xl">
                             Como você quer criar a sua conta no Psidevs?
                             </h3>
-                            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            <p class="text-lg leading-relaxed text-gray-500 dark:text-gray-400 lg:text-2xl">
                                 Selecione a opção que deseja criar a sua conta
                             </p>
                         </div>
@@ -23,24 +23,24 @@
                     </div>
 
                     <!-- Modal conteudo -->
-                    <div class="flex flex-col p-6 space-y-6 w-11/12 h-3/5 justify-center mx-auto">
-                        <button class= "h-2/5">
-                            <a href="cadastro-profissional.php?" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50  dark:border-gray-600 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white h-full">
-                                <img src="img/github.png" class="w-10 mr-4"  alt="">
+                    <div class="flex flex-col p-6 space-y-6 w-11/12 h-3/5 mt-2 sm:mb-10 content-center justify-items-center mx-auto lg:mt-6 sm:w-5/6 sm:h-4/6 ">
+                        <button class= "h-3/6">
+                            <a href="login.php?" class="flex items-center p-6 text-base font-bold text-gray-900 rounded-lg border border-gray-200 dark:border-gray-600 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white h-full">
+                            <img src="img/cliente-icon.svg" class="w-10 mr-4"  alt="">
                                 <div class="text-left">
-                                    <span class="flex-1">Cliente</span>
-                                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                    <span class="flex-1 text-xl lg:text-2xl">Cliente   |  Cadastro</span>
+                                    <p class="text-lg leading-relaxed text-gray-400 dark:text-gray-400 lg:text-xl">
                                     Quero fazer sessões de terapias e ver conteúdos sobre saúde emocional
                                     </p>
                                 </div>
                             </a>
                         </button>
-                        <button class= "h-2/5">
-                            <a href="cadastro-profissional.php?" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white h-full">
-                                <img src="img/github.png" class="w-10 mr-4"  alt="">
+                        <button class= "h-3/6">
+                        <a href="login.php?" class="flex items-center p-6 text-base font-bold text-gray-900 rounded-lg border border-gray-200 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white h-full">
+                                <img src="img/profissional-icon.svg" class="w-10 mr-4"  alt="">
                                 <div class="text-left">
-                                    <span class="flex-1">Profissional</span>
-                                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                    <span class="flex-1 text-xl lg:text-2xl">Profissional   |  Cadastro</span>
+                                    <p class="text-lg leading-relaxed text-gray-400 dark:text-gray-400 lg:text-xl">
                                     Quero atender pacientes online e fazer gestão da minha carreira
                                     </p>
                                 </div>
