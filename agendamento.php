6af8a9c8883c5b8225ec502e7a4345cb8d800ca7
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-10-30 10:05:48 -0300

    Card do psiclologo finalizado

--- agendamento.php
+++ agendamento.php
@@ -40,7 +40,7 @@
             </p>
         </section>
         <section class="filtros">
-            <form action="" method="get" >
+            <form action="" method="get">
                 <div>
                     <p>Tipo de Especialista</p>
                     <select name="especialista" class="form-select form-select-lg mb-3" id="especialista">
@@ -50,7 +50,7 @@
             </form>
 
             <form action="" method="get">
-            <div>
+                <div>
                     <p>Valor da sessão</p>
                     <select name="preco" id="preco" class="form-select form-select-lg mb-3">
                         <option value="">Qualquer valor</option>
@@ -58,29 +58,37 @@
                 </div>
             </form>
         </section>
-        
+
         <section class="container">
-            <article class="exibir-psicologo">
-              <div class="flex">
-                <div>
-                    <p><img src="https://cdn-icons-png.flaticon.com/512/711/711769.png" alt=""></p>
-                    <h3>nome</h3>
-                    <p><b>Preco</b></p>
-
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ipsa officia officiis sunt aperiam dolores eligendi iusto adipisci ea, dolore quam cum tenetur, possimus porro similique quis dolor amet ullam!</p>
-                    <h2>Plataformas</h2>
-                    <div class="flex">
-                        <p><img src="https://logodownload.org/wp-content/uploads/2015/04/whatsapp-logo-1.png" alt=""></p>
-                        <p><img src="https://logodownload.org/wp-content/uploads/2015/04/whatsapp-logo-1.png" alt=""></p>
-                        <p><img src="https://logodownload.org/wp-content/uploads/2015/04/whatsapp-logo-1.png" alt=""></p>
+            <div class="card-psicologo">
+                <!-- Inicio do card -->
+                <article class="exibir-psicologo d-md-flex justify-content-evenly gap-3">
+                    <div class="d-md-flex gap-5">
+                        <div>
+                            <div class="d-md-flex justify-content-evenly align-items-center perfil">
+                                <p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPORoC7j6v6eU8dw_jar82h7_bn_gkcPC9Kjxjj8Y4&s" alt=""></p>
+                                <div class="nome-preco">
+                                    <h3> Bar da Lora</h3>
+                                    <p><b>Preco</b></p>
+                                </div>
+                            </div>
+                            <div class="descricao">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ipsa officia officiis sunt aperiam dolores eligendi iusto adipisci ea, dolore quam cum tenetur, possimus porro similique quis dolor amet ullam!</p>
+                            </div>
+                            <h2>Plataformas:</h2>
+                            <div>
+                                <p class="d-flex icones "><img src="https://logodownload.org/wp-content/uploads/2015/04/whatsapp-logo-1.png" alt="">
+                                <img src="https://logodownload.org/wp-content/uploads/2015/04/whatsapp-logo-1.png" alt="">
+                                <img src="https://logodownload.org/wp-content/uploads/2015/04/whatsapp-logo-1.png" alt=""></p>
+                            </div>
+                        </div>
+                        <div class="agenda">
+                            <h2>Horarios Disponiveis</h2>
+                        </div>
                     </div>
-
-                </div>
-                <div>
-                    <h2>Horarios Disponiveis</h2>
-                </div>
-              </div>
-            </article>
+                </article>
+                <!-- Fim do card -->
+            </div>
         </section>
 
     </main>
