a930f6961d462fbf9dc06153d1521decb69e406d
Melissa Julia L. L <108154398+melissaJll@users.noreply.github.com>
2023-11-01 11:23:37 -0300

    texto arrumado

--- inc/modal-cadastro.php
+++ inc/modal-cadastro.php
@@ -30,7 +30,7 @@
                     <li >
                         <button class= "h-3/6 w-full">
                             <a href="cadastro-cliente.php?" class="flex items-center p-6 text-base font-semibold text-gray-900 rounded-lg border border-gray-200 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white h-full">
-                                <img src="img/profissional-icon.svg" class="w-10 mr-4"  alt="">
+                                <img src="img/cliente-icon.svg" class="w-10 mr-4" alt="">
                                 <div class="text-left">
                                 <span class="flex-1 text-2xl lg:text-3xl">Cliente   |  Cadastro</span>
                                     <p class="text-xl leading-relaxed text-gray-400 dark:text-gray-400 lg:text-2xl">
