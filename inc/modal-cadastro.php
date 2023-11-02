d79c2b4e54372cb73b17f31acd6e6082952094f1
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-11-02 12:13:14 -0300

    Update modal.php

--- inc/modal-cadastro.php
+++ inc/modal-cadastro.php
@@ -29,11 +29,11 @@
                 <ul class="my-4 space-y-3">
                     <li >
                         <button class= "h-3/6 w-full">
-                            <a href="cadastro-cliente.php?" class="flex items-center p-6 text-base  text-gray-700 rounded-lg border border-gray-200 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white h-full">
+                            <a href="cadastro-cliente.php?" class="flex items-center p-6 text-gray-800 rounded-lg border border-gray-200 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white h-full">
                                 <img src="img/cliente-icon.svg" class="w-10 mr-4" alt="">
                                 <div class="text-left font-ubuntu font-normal">
-                                <span class="flex-1 text-2xl font-semibold lg:text-3xl">Cliente   |  Cadastro</span>
-                                    <p class="text-xl leading-relaxed text-gray-400 dark:text-gray-400 lg:text-2xl">
+                                    <span class="flex-1 text-2xl font-semibold lg:text-3xl">Cliente   |  Cadastro</span>
+                                    <p class="text-xl leading-relaxed text-gray-500 dark:text-gray-400 lg:text-2xl">
                                     Quero fazer sessões de terapias e ver conteúdos sobre saúde emocional
                                     </p>
                                 </div>
@@ -42,11 +42,11 @@
                     </li>
                     <li>
                         <button class= "h-3/6 w-full">
-                            <a href="cadastro-profissional.php?" class="flex items-center p-6 text-base text-gray-700 rounded-lg border border-gray-200 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white h-full">
+                            <a href="cadastro-profissional.php?" class="flex items-center p-6 text-gray-800 rounded-lg border border-gray-200 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white h-full">
                                 <img src="img/profissional-icon.svg" class="w-10 mr-4"  alt="">
                                 <div class="text-left font-ubuntu font-normal">
                                 <span class="flex-1 text-2xl font-semibold lg:text-3xl">Profissional   |  Cadastro</span>
-                                    <p class="text-xl leading-relaxed text-gray-400 dark:text-gray-400 lg:text-2xl">
+                                <p class="text-xl leading-relaxed text-gray-500 dark:text-gray-400 lg:text-2xl">
                                     Quero atender pacientes online e fazer gestão da minha carreira
                                     </p>
                                 </div>
