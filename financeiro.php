a65f9144ba34de861cc209c1e800ba2cc62be5f8
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2023-11-08 22:41:06 -0300

    Logo novo favicon

--- financeiro.php
+++ financeiro.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Finanças | Psidevs online</title>
-    <link rel="stylesheet" href="css/style.css">.
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="assets/logo-favicon.png" type="image/x-icon">
 </head>
 <body>
   <header>
